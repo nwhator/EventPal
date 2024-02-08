a1469752746f87b784f0c3ea94f7b7c271177848
zaragood <azaragoodness52@gmail.com>
2024-02-08 01:02:02 +0000

    sservice section

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
     const toggleMenu = document.querySelector(".toggleMenu");
     const navigation = document.querySelector(".navigation");
+    const searchBtn = document.querySelector(".search-btn");
     toggleMenu.classList.toggle("active");
     navigation.classList.toggle("active");
+
+    // Check if the navigation is active
+    if (navigation.classList.contains("active")) {
+        searchBtn.style.display = "none"; // Hide the search button
+    } else {
+        searchBtn.style.display = "block"; // Show the search button
+    }
 }
